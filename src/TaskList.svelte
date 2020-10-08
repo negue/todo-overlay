76dbb67f27959bb3c077c8a3385ac9bbdc0252fa
negue <eugen.bolz@gmail.com>
2020-10-08 22:09:19 +0200

    test change

--- src/TaskList.svelte
+++ src/TaskList.svelte
@@ -44,7 +44,7 @@ export let layout = 'full';
 </script>
 
 <div class="nes-container {layout === 'auto' ? 'auto-layout' : ''} is-dark with-title">
-  <p class="title">{taskListName}</p>
+  <p class="title">Test Change {taskListName}</p>
 
   <div
     class="items-holder {highlightItemIndex !== -1 ? 'currently-highlighting' : '' }"
