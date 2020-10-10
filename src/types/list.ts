d65df5d9d89e9e2a102acb350c934d14db2c4c91
Steve Andreou <stephen.andreou@gmail.com>
2020-10-10 19:02:55 +1100

    TodoList type cut down

--- src/types/list.ts
+++ src/types/list.ts
@@ -1,12 +1,3 @@
-import type { TodoItem } from "./item";
-
 export interface TodoList {
-
   name?: string;
-  items?: TodoItem[];
-  highlightItemIndex?: number;
-  showOnlyItemIndex?: number;
-  scrollingInterval?: number;
-  scrollingDuration?: number;
-  layout?: string;
 }
